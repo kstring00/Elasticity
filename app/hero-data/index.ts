37a4b182ec6c736df9b54ecb986af688aecd6254
kstring00 <106356792+kstring00@users.noreply.github.com>
2026-08-30 15:48:08 -0500

    Assemble exact sunset hero image

--- app/hero-data/index.ts
+++ app/hero-data/index.ts
@@ -1,5 +1,9 @@
 import part1 from './part1'
 import part2 from './part2'
 import part3 from './part3'
+import part4 from './part4'
+import part5 from './part5'
+import part6 from './part6'
+import part7 from './part7'
 
-export const HERO_COMPOSITE_DATA_URI = `data:image/webp;base64,${part1}${part2}${part3}`
+export const HERO_COMPOSITE_DATA_URI = `data:image/webp;base64,${part1}${part2}${part3}${part4}${part5}${part6}${part7}`
