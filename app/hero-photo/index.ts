bdbaaa5d44b85440bac1608ff1e1921a5215f8ff
kstring00 <106356792+kstring00@users.noreply.github.com>
2026-08-30 15:44:29 -0500

    Assemble supplied lakeside hero image

---
+++ app/hero-photo/index.ts
@@ -0,0 +1,7 @@
+import part1 from './part1'
+import part2 from './part2'
+import part3 from './part3'
+import part4 from './part4'
+import part5 from './part5'
+
+export const HERO_PHOTO_DATA_URI = `data:image/webp;base64,${part1}${part2}${part3}${part4}${part5}`
